049967dcfa4aa18121c199f94bf3d99f5cff3692
Admir Ferhatović <admir.ferhatovic@gmail.com>
2019-04-28 15:32:33 +0200

    Added cat csv

--- HyperLogLog/main.cpp
+++ HyperLogLog/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "HyperLogLog.h"
 
@@ -6,7 +7,7 @@ using namespace std;
 
 int main()
 {
-	for (auto i = 1000; i < 100000; i += 1000) {
+	for (auto i = 1000; i < 10000; i += 200) {
 		hll::HyperLogLog hll(true, [i](double e) {
 			string line = to_string(i) + ", " + to_string(e) + "\n";
 			return line;
@@ -14,7 +15,28 @@ int main()
 		hll.test(i);
 	}
 
-	cout << "Finished..." << endl;
+	cout << "Finished graph data..." << endl;
+
+	ifstream csv;
+	csv.open("categories.csv");
+
+	if (!csv) {
+		cout << "No file found...";
+		return 1;
+	}
+
+	int item_count = 0;
+	string word;
+	hll::HyperLogLog hll;
+
+	while (csv >> word) {
+		hll.add(word.c_str());
+		++item_count;
+	}
+
+	cout << "CSV count(" << item_count << "): " << hll.count() << endl;
+
+	csv.close();
 
 	cin.get();
 
